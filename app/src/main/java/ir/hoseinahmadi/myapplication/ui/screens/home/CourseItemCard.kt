c248222bda43b7a26d2a012e239068ef8d849ef7
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-07-26 12:49:10 +0330

    fix CourseItemCard

--- app/src/main/java/ir/hoseinahmadi/myapplication/ui/screens/home/CourseItemCard.kt
+++ app/src/main/java/ir/hoseinahmadi/myapplication/ui/screens/home/CourseItemCard.kt
@@ -1,5 +1,6 @@
 package ir.hoseinahmadi.myapplication.ui.screens.home
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -9,11 +10,14 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import ir.hoseinahmadi.myapplication.data.model.CourseItem
@@ -22,26 +26,35 @@ import ir.hoseinahmadi.myapplication.data.model.CourseItem
 @Composable
 fun CourseItemCard(item: CourseItem) {
     Card(
+        modifier = Modifier.padding(12.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         ),
         elevation = CardDefaults.cardElevation(2.dp),
         onClick = { }) {
-        GlideImage(
-            modifier = Modifier
-                .padding(5.dp)
-                .size(330.dp, 190.dp)
-                .clip(RoundedCornerShape(12.dp)),
-            model = item.image,
-            contentDescription = ""
-        )
-        Text(
-            modifier = Modifier.padding(5.dp),
-            text = item.title,
-            style = MaterialTheme.typography.bodyLarge,
-            fontWeight = FontWeight.SemiBold,
-            color = Color.Blue
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            GlideImage(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .size(330.dp, 190.dp),
+                model = item.image,
+                contentDescription = "",
+                contentScale = ContentScale.Crop
             )
+            Text(
+                modifier = Modifier.padding(10.dp),
+                text = item.title,
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontSize = 15.sp
+                ),
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+        }
+
     }
 
 }
