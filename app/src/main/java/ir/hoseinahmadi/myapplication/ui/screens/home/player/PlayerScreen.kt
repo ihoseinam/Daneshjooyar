c9b509c9dd0c80348b8471030e71ae863f110792
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-07-27 11:20:47 +0330

    create PlayerScreen

--- app/src/main/java/ir/hoseinahmadi/myapplication/ui/screens/home/player/PlayerScreen.kt
+++ app/src/main/java/ir/hoseinahmadi/myapplication/ui/screens/home/player/PlayerScreen.kt
@@ -1,7 +1,17 @@
 package ir.hoseinahmadi.myapplication.ui.screens.home.player
 
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.SecureFlagPolicy
+import androidx.media3.exoplayer.analytics.AnalyticsListener
 import androidx.navigation.NavHostController
+import io.sanghun.compose.video.RepeatMode
+import io.sanghun.compose.video.VideoPlayer
+import io.sanghun.compose.video.controller.VideoPlayerControllerConfig
+import io.sanghun.compose.video.uri.VideoPlayerMediaItem
 
 @Composable
 fun PlayerScreen(
@@ -9,5 +19,49 @@ fun PlayerScreen(
     data: String,
     videoIndex: Int,
 ) {
+
+    VideoPlayer(
+        mediaItems = listOf<VideoPlayerMediaItem>(
+            VideoPlayerMediaItem.NetworkMediaItem(
+                data
+            )
+        ),
+        handleLifecycle = true,
+        autoPlay = false,
+        usePlayerController = true,
+        enablePip = false,
+        handleAudioFocus = true,
+        controllerConfig = VideoPlayerControllerConfig(
+            showSpeedAndPitchOverlay = true,
+            showSubtitleButton = false,
+            showCurrentTimeAndTotalTime = true,
+            showBufferingProgress = true,
+            showForwardIncrementButton = true,
+            showBackwardIncrementButton = true,
+            showBackTrackButton = false,
+            showNextTrackButton = false,
+            showRepeatModeButton = false,
+            controllerShowTimeMilliSeconds = 5_000,
+            showFullScreenButton = true,
+            controllerAutoShow = true,
+        ),
+        volume = 0.6f,  // volume 0.0f to 1.0f
+        repeatMode = RepeatMode.NONE,       // or RepeatMode.ALL, RepeatMode.ONE
+        onCurrentTimeChanged = { // long type, current player time (millisec)
+            Log.e("CurrentTime", it.toString())
+        },
+        enablePipWhenBackPressed = true,
+        fullScreenSecurePolicy = SecureFlagPolicy.SecureOn, // فعال کردن حالت ایمن
+        playerInstance = { // ExoPlayer instance (Experimental)
+            addAnalyticsListener(
+                object : AnalyticsListener {
+                    // player logger
+                }
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    )
 
 }
